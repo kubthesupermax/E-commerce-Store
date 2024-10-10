73591b468d44bc201fcba1a49b99d9ca41fab838
Franklin Kuba <franklinkuba7@gmail.com>
2024-10-10 13:58:11 -0600

    update

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,5 +1,19 @@
+import User from "../models/user.model.js";
+
 export const signup = async (req, res) => {
   const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+
+  const user = await User.create({ name, email, password });
+
+  res.status(201).json({
+    user,
+    message: "User created successfully",
+  });
   res.send("signup route called");
 };
 
